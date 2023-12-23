a83fced0b1086e623d06e7a4be007003861905ae
Nayal17 <nayal17.916@gmail.com>
2023-12-23 16:03:55 +0530

    added data prep entity

--- src/SolarPanelDetection/entity/config_entity.py
+++ src/SolarPanelDetection/entity/config_entity.py
@@ -7,3 +7,17 @@ class DataIngestionConfig:
     source_URL: str
     local_data_file: Path
     unzip_dir: Path
+
+@dataclass(frozen=True)
+class DataPreparationConfig:
+    features: list
+    img_dir: Path
+    mask_dir: Path
+    dataframe_save_path: Path
+
+@dataclass(frozen=True)
+class TrainingConfig:
+    features: list
+    n_splits: int
+    lgb_params: dict
+    model_save_path: Path
